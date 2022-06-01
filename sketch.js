0fc38ea397bce8a4d5c649050d7665434d8fb7f7
Discono <44101641+Discono@users.noreply.github.com>
2022-06-01 14:23:17 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,40 @@
 function setup() {
-  // put setup code here
+size(640,480);
+
+String[] cams = Capture.list();
+println(cams[0]+"test");
+
+
+camera = new Capture(this, cams[0]);
+camera.start();
+img = camera;
 }
 
 function draw() {
   // put drawing code here
+  image(img, 0, 0);
+}
+
+//do note that i wasn't able to test this at home, so it may nd up failing completely and i will have no idea.
+import processing.video.*;
+Capture camera;
+PImage img;
+
+function captureEvent(Capture camera){
+ camera.read(); 
+}
+
+
+function keyPressed() {
+if (key == ' ') {
+invert();
+
+}
+}
+
+function invert() {
+  for (int p = 0; p<img.pixels.length; p++) {
+    img.pixels[p] = color(abs(255 - red(img.pixels[p])), abs(255 - blue(img.pixels[p])), abs(255 - green(img.pixels[p])));
+  }
+  img.updatePixels();
 }
